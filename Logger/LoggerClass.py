43e010db9f6248c2e2133cb15415cec8c8be3147
D <didier.froids@gmail.com>
2025-09-04 23:38:30 +0200

    feat: Added Logger class
    	+ Added Logger class
    	+ Added Color class
    
    NOTE: 	The logger is using the `typing` lib to be compatible
    		with Python <= 3.9

---
+++ Logger/LoggerClass.py
@@ -0,0 +1,74 @@
+from typing import List, Dict, Tuple # For Python before 3.9
+from ColorClass import col
+
+# ┌───────────────────────────────────┐
+# │            TAGS_COLORS            │
+# └───────────────────────────────────┘
+
+tags_color = {
+    "INFO":     [col.bg.CYA, col.BLA, col.BOLD],
+    "WARN":     [col.bg.YEL, col.BLA, col.BOLD],
+    "INFO ":    [col.bg.CYA, col.BLA, col.BOLD], # For alignment
+    "WARN ":    [col.bg.YEL, col.BLA, col.BOLD], # For alignment
+    "ERROR":    [col.bg.RED, col.BLA, col.BOLD],
+    "DEBUG":    [col.bg.BLU, col.BLA, col.BOLD],
+    "DOWNLOAD": [col.bg.PUR, col.BLA, col.BOLD],
+    "DONE":     [col.bg.GRE, col.BLA, col.BOLD],
+    "TIME":     [col.bg.BLU, col.BLA, col.BOLD],
+}
+
+################################################################################
+
+
+def log(msg: str, colors: List[str], end: str = "\n") -> None:
+    for x in colors:
+        print(x, end="")
+    print(f"{msg}{col.RES}", end=end)
+
+def log_tags(msg: str, tags: List[Tuple[str, List[str]]]) -> None:
+    for tag in tags:
+        log(f" {tag[0]} ", tag[1], "")
+    print("", msg)
+
+
+def log_t(msg: str, tags: List[str] | str) -> None:
+    """
+    This function will print tags before the message with color codes defined in
+    the tags_color global var.
+    You should maybe use the Logger class.
+    """
+    if type(tags) == str:
+        tags = [tags]
+    for t in tags:
+        # print(tags_color.get(t, []))
+        if t in tags_color:
+            log(f" {t} ", tags_color.get(t, []), "")
+            # log(f" {t:<8} ", tags_color.get(t, []), "")
+        else:
+            log(f"[ {t} ]", [], "")
+    print("", msg)
+
+
+# Logger class
+# @param    msg     The message to be printed
+# @param    tags    Take the tag (or list of tags) to be printed before 
+class Logger:
+    def info(self, msg : str, tags: List[str] | str = []) -> None:
+        if type(tags) == str:
+            tags = [tags]
+        log_t(msg, ["INFO "] + tags) # type: ignore
+
+    def warn(self, msg : str, tags: List[str] | str  = []) -> None:
+        if type(tags) == str:
+            tags = [tags]
+        log_t(msg, ["WARN "] + tags) # type: ignore
+
+    def error(self, msg : str, tags: List[str] | str  = []) -> None:
+        if type(tags) == str:
+            tags = [tags]
+        log_t(msg, ["ERROR"] + tags) # type: ignore
+
+    def debug(self, msg : str, tags: List[str] | str  = []) -> None:
+        if type(tags) == str:
+            tags = [tags]
+        log_t(msg, ["DEBUG"] + tags) # type: ignore
